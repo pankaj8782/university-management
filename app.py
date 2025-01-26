24b8ebf6f7c8c8067a3b88f73db69255cdc8ce2c
Pankaj <pankaj8782.upl@gmail.com>
2025-01-26 16:54:10 +0530

    Resolved OS Module

--- app.py
+++ app.py
@@ -141,10 +141,8 @@ def contact_us():
     return render_template('contact_us.html')
 
 
-<<<<<<< HEAD
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
-=======
-if __name__ == '__main__':
-    app.run(debug=True)
->>>>>>> 22075188181718c015e6e3cdb98ce6366d556c32
+
+
